0d8b3ef17a02b8ec198c7ea9e721b99e6f094b6a
AI-ANK <130707158+AI-ANK@users.noreply.github.com>
2024-03-21 10:23:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -202,4 +202,4 @@ async def redirect_root_to_docs():
 # Run the server
 if __name__ == "__main__":
   import uvicorn
-  uvicorn.run(app, host="0.0.0.0", port=8000)
+  uvicorn.run(app, host="0.0.0.0", port=80)
